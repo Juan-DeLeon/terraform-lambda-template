9db87bd0191620ba9d936bc70ff3f41ff0a7f181
Juan De Leon <juan.deleon@definityfirst.com>
2021-07-23 16:29:35 -0500

    added tag to ignored changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,7 +52,7 @@ resource "aws_lambda_function" "func" {
   }
   lifecycle {
     prevent_destroy = true
-    ignore_changes  = [layers, timeout]
+    ignore_changes  = [layers, timeout, tags]
   }
 }
 
